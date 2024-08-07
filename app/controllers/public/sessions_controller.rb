4d65b96584b49e3db601fb5b24171b55f07aeda6
Ogawa <ogawadaiki@ogawadaikinoMacBook-Air.local>
2024-08-07 11:48:57 +0900

    管理者バグ修正

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -30,6 +30,6 @@ class Public::SessionsController < Devise::SessionsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_in_params
-    devise_parameter_sanitizer.permit(:sign_in, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:email, :name])
   end
 end
