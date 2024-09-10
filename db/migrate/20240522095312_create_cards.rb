d6ee2aaa741b167bee2ccd5cb87a4b4863def8c7
Ogawa <ogawadaiki@ogawadaikinoMacBook-Air.local>
2024-09-11 00:24:19 +0900

    修正：職業欄とレイアウト

--- db/migrate/20240522095312_create_cards.rb
+++ db/migrate/20240522095312_create_cards.rb
@@ -3,6 +3,7 @@ class CreateCards < ActiveRecord::Migration[7.0]
     create_table :cards do |t|
       t.string :name, null: false
       t.integer :brand, null: false
+      t.integer :money, default: 0
       t.string :purpose, default: ""
 
       t.references :user, foreign_key: true, null: false
